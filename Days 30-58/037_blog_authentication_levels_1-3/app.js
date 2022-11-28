b0d3c065c62a60744ac5deeddb08a94c69c949d1
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-11-28 16:32:58 -0700

    Day 37
    Day 37 - Blog Authentication, Levels 1-3
    * Worked with a standard text password, encrypted password, .env stored password, and hashed password.
    * Wrote the node.js code to make each of these passwords work properly and be stored.
    * Investigated how to create stronger passwords and hashing to protect those passwords.

---
+++ Days 30-58/037_blog_authentication_levels_1-3/app.js
@@ -0,0 +1,83 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const md5 = require('md5')
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+}
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+  });
+
+
+
+  const User = mongoose.model("User", userSchema);
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+
+app.post('/login', function(req, res) {
+    const username = req.body.username;
+    const password = md5(req.body.password);
+
+    User.findOne({email: username}, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (foundUser.password === password) {
+                res.render('secrets');
+            }
+        };
+    });
+});
+
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+app.post('/register', function(req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: md5(req.body.password)
+    });
+    newUser.save(function(err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render('secrets');
+        }
+    });
+});
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000.")
+})
