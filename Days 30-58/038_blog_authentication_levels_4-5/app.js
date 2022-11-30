cc3dcd8ef4ca06bc1950b32f948aed96ff206728
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-11-29 23:11:46 -0700

    Day 38
    Day 38 - Blog Authentication, Levels 4-5
    * Learned to salt each of the hashed passwords with md5 in Node Js.
    * Also tracked sessions for each user, allowing for users to access secret pages, even after navigating away.
    * Created a logout method to end the users' session, as well as a button to facilitate that action.

---
+++ Days 30-58/038_blog_authentication_levels_4-5/app.js
@@ -0,0 +1,130 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+}
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+  });
+
+  userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+////////////////////////////// MAIN SITE ////////////////////////////
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+////////////////////////////// REGISTER ////////////////////////////
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+app.post('/register', function(req, res) {
+
+    User.register({username:req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect('/register');
+        }
+        else {
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/secrets');
+            });
+        }
+    });
+
+});
+
+////////////////////////////// LOGIN ////////////////////////////////
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+
+app.post('/login', function(req, res) {
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if (err) {
+            console.log(err);
+        }
+        else {
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/secrets');
+            });
+        }
+    });
+});
+
+
+////////////////////////////// LOGOUT ////////////////////////////////
+
+app.get('/logout', function(req, res){
+    req.logout((err) =>{
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.redirect('/');
+        }
+    });
+});
+
+
+////////////////////////////// SECRETS ///////////////////////////////
+app.get('/secrets', function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render('secrets');
+    }
+    else {
+        res.redirect('/login');
+    }
+})
+
+
+
+////////////////////////////// LISTEN /////////////////////////////
+app.listen(3000, function() {
+    console.log("Server started on port 3000.")
+})
