c50adf718b6813808cbdf63425fc83a9138895bc
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-12-03 06:05:00 -0700

    Day 41
    Day 41 - Keeper v.1
    * Practiced methods for organizing a React App with naming conventions, imports, exports, and file organization.
    * Created the beginning of the Keeper App with each of these methods as a React App.
    * Keeper is meant to be a Google Keeps clone and does not yet have any functionality.

---
+++ Days 30-58/041_keeper_v1/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Heading from './Header';
+import Footer from './Footer';
+import Note from './Note';
+
+function App() {
+    return (
+      <div>
+        <Heading />
+        <Note />
+        <Footer />
+      </div>
+    );
+  }
+
+  export default App;
---
+++ Days 30-58/041_keeper_v1/my-app/src/Note.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Note() {
+    return (
+        <div className='note'>
+            <h1>Title</h1>
+            <p>Content</p>
+        </div>
+    )
+}
+
+export default Note;
+
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
